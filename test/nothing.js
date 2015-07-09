793871a9a125e1f3f7257d75b7025db3d2fc6901
Rory Walsh <rorywwalsh@users.noreply.github.com>
2015-07-09 15:20:24 +0100

    add a test

---
+++ test/nothing.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+
+describe('addition', function () {
+ it('should add 1+1 correctly', function () {
+   var onePlusOne = 1 + 1;
+   assert.equal(onePlusOne, 2);
+ });
+});
