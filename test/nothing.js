e61976b152170d91093543926a3291a76ea266a6
Rory Walsh <rorywwalsh@users.noreply.github.com>
2015-07-10 14:23:57 +0100

    fix test

--- test/nothing.js
+++ test/nothing.js
@@ -5,6 +5,6 @@ var assert = require('assert');
 describe('addition', function () {
  it('should add 1+1 correctly', function () {
    var onePlusOne = 1 + 1;
-   assert.equal(onePlusOne, 3);
+   assert.equal(onePlusOne, 2);
  });
 });
